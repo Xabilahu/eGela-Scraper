b055a16c65b71f8f86a28666f1f968bac009b6ef
xabilahu <xabilahu99@hotmail.com>
2020-04-09 02:09:35 +0200

    Virtual environment activation and deactivation

--- scrap.sh
+++ scrap.sh
@@ -4,6 +4,7 @@ setup() {
     virtualenv --python=python3 env
     source env/bin/activate
     pip install -r res/requirements.txt
+    deactivate
 }
 
 if [ -z "$(dpkg -l | grep virtualenv)" ]; then
@@ -14,4 +15,6 @@ if [ -z "$(ls env 2> /dev/null)" ]; then
     setup
 fi
 
+source env/bin/activate
 python3 src/scrap.py "$@"
+deactivate
